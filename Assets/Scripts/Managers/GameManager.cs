1affb610924fc33809362aeabd17fe6aa0907bc5
Infernaton <lubin.pauly@gmail.com>
2024-09-19 11:40:47 +0200

    Create Instanciate for GameManager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public static GameManager Instance; // A static reference to the GameManager instance
+
+    void Awake()
+    {
+        if (Instance == null) // If there is no instance already
+        {
+            Instance = this;
+        }
+        else if (Instance != this) // If there is already an instance and it's not `this` instance
+            Destroy(gameObject); // Destroy the GameObject, this component is attached to
+    }
+
     void Start()
     {
         
