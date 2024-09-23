9833862013e7744fc370be753a699957c0eadd08
MaximeJomain <maxime.jomain95@gmail.com>
2024-09-23 10:42:38 +0200

    fix setrock

--- Assets/Scripts/SetRock.cs
+++ Assets/Scripts/SetRock.cs
@@ -8,15 +8,17 @@ public class SetRock : MonoBehaviour
     [SerializeField]  List<int> positions = new List<int>();
     [SerializeField] List<Material> rocksMaterials = new List<Material>();
     [SerializeField] bool _isGoldRock;
+    [SerializeField] GameObject _gfx;
+    [SerializeField] MeshRenderer _meshRenderer;
+    
     void Start()
     {
 
         if (!_isGoldRock)
         {
-            this.gameObject.GetComponent<MeshRenderer>().material = rocksMaterials[Random.Range(0, rocksMaterials.Count - 1)];
-
+            _meshRenderer.material = rocksMaterials[Random.Range(0, rocksMaterials.Count - 1)];
+            _gfx.transform.rotation = Quaternion.Euler(0, 0, Random.Range(0, positions[Random.Range(0, positions.Count-1)]));
         }
-        this.gameObject.transform.rotation = Quaternion.Euler(0, 0, Random.Range(0, positions[Random.Range(0, positions.Count-1)]));
     }
 
     void Update()
