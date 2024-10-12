f0dd4db1b418dcf9ff1b43a079fa4416f393c27b
Alexis <alexispelissier2002@gmail.com>
2024-10-12 13:35:46 +0200

    add Cart Sound

--- Assets/Scripts/Objects/GoldChariot/PushChariot.cs
+++ Assets/Scripts/Objects/GoldChariot/PushChariot.cs
@@ -1,16 +1,35 @@
+using FMOD.Studio;
+using FMODUnity;
 using UnityEngine;
 
 public class PushChariot : MonoBehaviour
 {
     [SerializeField] private float pushForce = 150;
     [SerializeField] private Rigidbody chariotRigidbody;
+    [SerializeField] private EventReference chariotSound;
+    private EventInstance _chariotEventInstance;
+    private SoundUtils soundUtils;
 
-    private bool _isTriggerActive;
+    private bool _isTriggerActive = false;
     private PlayerFatigue _playerFatigue;
+    private bool _isSoundPlaying = false;
 
+    void Start()
+    {
+        soundUtils = gameObject.AddComponent<SoundUtils>();
+    }
 
     private void FixedUpdate()
     {
+        if (chariotRigidbody.velocity.x > 0)
+        {
+            PlayChariotSound();
+        }
+        else
+        {
+            PauseChariotSound();
+        }
+
         if (!_isTriggerActive || _playerFatigue == null) return;
 
         if (_playerFatigue.ReduceCartsFatigue(10f * Time.deltaTime))
@@ -21,7 +40,7 @@ public class PushChariot : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (Utils.TryGetParentComponent<PlayerActions>(other, out var player) && !player.isHoldingObject)
+        if (Utils.TryGetParentComponent<PlayerActions>(other, out var player) && !player.isHoldingObject && !_isTriggerActive)
         {
             _isTriggerActive = true;
             _playerFatigue = player.playerFatigue;
@@ -30,11 +49,54 @@ public class PushChariot : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (Utils.TryGetParentComponent<PlayerActions>(other, out _))
+        if (Utils.TryGetParentComponent<PlayerActions>(other, out _) && _isTriggerActive)
         {
             _isTriggerActive = false;
             _playerFatigue = null;
         }
     }
 
+    #region SFX
+    private void PlayChariotSound()
+    {
+        if (!_isSoundPlaying)
+        {
+            if (!_chariotEventInstance.isValid())
+            {
+                _chariotEventInstance = RuntimeManager.CreateInstance(chariotSound);
+                _chariotEventInstance.set3DAttributes(RuntimeUtils.To3DAttributes(transform));
+                _chariotEventInstance.start();
+            }
+            else
+            {
+                soundUtils.UnpauseWithFade(_chariotEventInstance, 0.1f);
+            }
+            
+            _isSoundPlaying = true;
+        }
+    }
+
+    public void PauseChariotSound()
+    {
+        if (_isSoundPlaying)
+        {
+            FMOD.RESULT result = _chariotEventInstance.getPaused(out bool isPaused);
+            if (result == FMOD.RESULT.OK && !isPaused)
+            {
+                soundUtils.PauseWithFade(_chariotEventInstance, 0.1f);
+                _isSoundPlaying = false;
+            }
+        }
+    }
+
+    public void StopChariotSound()
+    {
+        if (_chariotEventInstance.isValid())
+        {
+            _chariotEventInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            _chariotEventInstance.release();
+            _isSoundPlaying = false;
+        }
+    }
+    #endregion
 }
