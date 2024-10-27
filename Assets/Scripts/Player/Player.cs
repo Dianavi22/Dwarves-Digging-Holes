4b626fe1ad059a781789073044ed6313c4c835cf
Infernaton <lubin.pauly@gmail.com>
2024-10-27 19:14:08 +0100

    re-add Player.cs. The file was on the gitignore list :expressionless:

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,34 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/**
+ * @todo Need to better instanciate this for better performance
+ * For now - each child will do the Awake. Better if can use SerializeField only once where each child can have reference to
+ */
+public class Player : MonoBehaviour
+{
+    protected PlayerMovements movements;
+    protected PlayerActions actions;
+    protected PlayerHealth health;
+    protected PlayerFatigue fatigue;
+    protected UserInput input;
+    protected Rigidbody rb;
+
+    private void Awake()
+    {
+        movements = GetComponent<PlayerMovements>();
+        actions = GetComponent<PlayerActions>();
+        health = GetComponent<PlayerHealth>();
+        fatigue = GetComponent<PlayerFatigue>();
+        input = GetComponent<UserInput>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public PlayerMovements GetMovement() => movements;
+    public PlayerActions GetActions() => actions;
+    public PlayerHealth GetHealth() => health;
+    public PlayerFatigue GetFatigue() => fatigue;
+    public UserInput GetInput() => input;
+    public Rigidbody GetRigidbody() => rb;
+}
