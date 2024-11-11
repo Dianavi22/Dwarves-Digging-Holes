b26e21dd116b80081b70fbea2a3951d96b74fc25
Infernaton <lubin.pauly@gmail.com>
2024-11-11 17:02:52 +0100

    fix: Can't create new pickaxe
    `NbPickaxe--` wasn't doing much

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -26,7 +26,6 @@ public class GameManager : MonoBehaviour
         get => _nbPickaxe;
         set
         {
-            if (!CanCreatePickaxe) return;
             _nbPickaxe = value;
         }
     }
