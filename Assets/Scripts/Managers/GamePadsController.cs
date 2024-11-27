6719f67161fe55cdd515e60bf56e5e51df6aa6e8
MaximeJomain <72390612+MaximeJomain@users.noreply.github.com>
2024-11-27 14:57:42 +0100

    fix retry game pickaxe not respawning

--- Assets/Scripts/Managers/GamePadsController.cs
+++ Assets/Scripts/Managers/GamePadsController.cs
@@ -30,12 +30,9 @@ public class GamePadsController : MonoBehaviour
             Destroy(gameObject);
 
         PlayerList = new List<Player>();
-    }
-
-    void Start()
-    {
+        
         var gamepads = Gamepad.all;
-        //Debug.Log($"Number of gamepads: {gamepads.Count}");
+        // Debug.Log($"Number of gamepads: {gamepads.Count}");
 
         if(IsDebugMode) {
 
