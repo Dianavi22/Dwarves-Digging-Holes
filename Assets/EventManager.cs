a118a7d81eea60926f20903c64770437cbca6745
Jado <jade.bouiges@edu.esiee-it.fr>
2024-12-03 15:05:02 +0100

    add eventLava

--- Assets/EventManager.cs
+++ Assets/EventManager.cs
@@ -6,6 +6,9 @@ public class EventManager : MonoBehaviour
 {
     private bool _readyToEvent = true;
     [SerializeField] GoldChariot _goldChariot;
+    [SerializeField] GameObject _lava;
+    private bool _isLavaMove = false;
+    private bool _isLavaMoveEndEvent = false;
     void Start()
     {
         
@@ -17,22 +20,30 @@ public class EventManager : MonoBehaviour
         {
             StartCoroutine(Event());
         }
+        if (_isLavaMove)
+        {
+            _lava.transform.position = Vector3.Lerp(_lava.transform.position, new Vector3(_lava.transform.position.x + 5, _lava.transform.position.y, _lava.transform.position.z), Time.deltaTime * 0.5f);
+        }
+        if (_isLavaMoveEndEvent)
+        {
+            _lava.transform.position = Vector3.Lerp(_lava.transform.position, new Vector3(_lava.transform.position.x - 5, _lava.transform.position.y, _lava.transform.position.z), Time.deltaTime * 0.5f);
+        }
     }
 
     private IEnumerator Event()
     {
         _readyToEvent = false;
         yield return new WaitForSeconds(3);
-        print("EVENT");
+        StartCoroutine(LavaGetingClose());
         EventGoldChariot();
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(30);
         print("END EVENT");
         _readyToEvent = true;
     }
 
     //Event 1 : All pickaxes are Destroyed
     //Event 2 : if gold is > 10 : gold/2
-    //Event 3 : Lava getting cloth
+    //Event 3 : Lava getting close
     //Event 4 : Too many goblins
 
     private void EventPickaxe()
@@ -47,5 +58,18 @@ public class EventManager : MonoBehaviour
     private void EventGoldChariot()
     {
         _goldChariot.GoldEvent();
+    }
+
+    private IEnumerator LavaGetingClose()
+    {
+        _isLavaMove = true;
+        yield return new WaitForSeconds(4.5f);
+        _isLavaMove = false;
+        yield return new WaitForSeconds(4.5f);
+        _isLavaMoveEndEvent = true;
+        yield return new WaitForSeconds(4.5f);
+        _isLavaMoveEndEvent = false;
+
+
     }
 }
