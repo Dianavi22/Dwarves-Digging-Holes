75946e132b57893f2ab36a7a631c49e62802b9cf
Jado <jade.bouiges@edu.esiee-it.fr>
2024-12-03 16:42:26 +0100

    fix scroll lava event

--- Assets/EventManager.cs
+++ Assets/EventManager.cs
@@ -5,28 +5,35 @@ using UnityEngine;
 public class EventManager : MonoBehaviour
 {
     private bool _readyToEvent = true;
-    [SerializeField] GoldChariot _goldChariot;
+    private GoldChariot _goldChariot;
+    private float _scrollSpeed;
     [SerializeField] GameObject _lava;
     private bool _isLavaMove = false;
     private bool _isLavaMoveEndEvent = false;
+    private Vector3 _lavaPosition;
     void Start()
     {
-        
+        _goldChariot = TargetManager.Instance.GetGameObject<GoldChariot>(Target.GoldChariot);
+        _scrollSpeed = GameManager.Instance.Difficulty.ScrollingSpeed;
     }
     
     void Update()
     {
         if (_readyToEvent)
         {
-            StartCoroutine(Event());
+            //StartCoroutine(Event());
         }
         if (_isLavaMove)
         {
-            _lava.transform.position = Vector3.Lerp(_lava.transform.position, new Vector3(_lava.transform.position.x + 5, _lava.transform.position.y, _lava.transform.position.z), Time.deltaTime * 0.5f);
+            _lava.transform.position = Vector3.Lerp(_lava.transform.position, new Vector3(_lava.transform.position.x + 4, _lava.transform.position.y, _lava.transform.position.z), Time.deltaTime * 0.5f);
         }
         if (_isLavaMoveEndEvent)
         {
-            _lava.transform.position = Vector3.Lerp(_lava.transform.position, new Vector3(_lava.transform.position.x - 5, _lava.transform.position.y, _lava.transform.position.z), Time.deltaTime * 0.5f);
+            _lava.transform.position = Vector3.Lerp(_lava.transform.position, new Vector3(_lava.transform.position.x - 4, _lava.transform.position.y, _lava.transform.position.z), Time.deltaTime * _scrollSpeed/2);
+            if(_lava.transform.position.x <= _lavaPosition.x)
+            {
+                _isLavaMoveEndEvent = false;
+            }
         }
     }
 
@@ -62,13 +69,13 @@ public class EventManager : MonoBehaviour
 
     private IEnumerator LavaGetingClose()
     {
+        _lavaPosition = _lava.transform.position;
         _isLavaMove = true;
         yield return new WaitForSeconds(4.5f);
         _isLavaMove = false;
         yield return new WaitForSeconds(4.5f);
         _isLavaMoveEndEvent = true;
-        yield return new WaitForSeconds(4.5f);
-        _isLavaMoveEndEvent = false;
+       // yield return new WaitForSeconds(4.5f);
 
 
     }
