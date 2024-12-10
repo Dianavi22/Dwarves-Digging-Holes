aa7b87697f6b87fdfd75258ac6e4771fd4c34a4b
Alexis <alexispelissier2002@gmail.com>
2024-12-10 13:27:08 +0100

    fix goblin throw

--- Assets/Scripts/Entity/Entity.cs
+++ Assets/Scripts/Entity/Entity.cs
@@ -8,14 +8,30 @@ public abstract class Entity : MonoBehaviour, IGrabbable
     public bool IsGrabbed { get; protected set; }
     public float recoveryTime = 0.5f;
 
-    public virtual void HandleCarriedState(Player currentPlayer, bool isGrabbed) {
-        if(isGrabbed) {
+    private Tween recoveryTween;
+
+    public virtual void HandleCarriedState(Player currentPlayer, bool isGrabbed)
+    {
+        if (isGrabbed)
+        {
+            HandleTween();
             IsGrabbed = isGrabbed;
         }
-        else {
-            DOVirtual.DelayedCall(recoveryTime, () =>  IsGrabbed = isGrabbed);
+        else
+        {
+            HandleTween();
+            recoveryTween = DOVirtual.DelayedCall(recoveryTime, () => { IsGrabbed = isGrabbed; recoveryTween = null; });
         }
-        
+
+    }
+
+    private void HandleTween()
+    {
+        if (recoveryTween != null)
+        {
+            recoveryTween.Kill();
+            recoveryTween = null;
+        }
     }
 
     public virtual void HandleDestroy()
