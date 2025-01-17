bfe736ee423d3575e91c39d1049409d939c5f8c0
Alexis <alexispelissier2002@gmail.com>
2025-01-17 13:20:42 +0100

    return button - reformat

--- Assets/GameSettingsSelection.cs
+++ Assets/GameSettingsSelection.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
@@ -9,78 +8,81 @@ using Utils;
 
 public class GameSettingsSelection : MonoBehaviour
 {
+    #region Variables
 
-    #region Difficulty Variables
+    [Header("Difficulty Settings")]
     public Difficulty[] difficultyList;
     public TMP_Text difficultyText;
-    private Difficulty selectedDifficulty;
     private int selectedDifficultyIndex = 0;
 
-    #endregion
-
-    #region Mode Variables
-    private int selectedModeIndex = 0;
-    private GameMode selectedMode;
+    [Header("Mode Settings")]
     public TMP_Text modeText;
+    private int selectedModeIndex = 0;
+
     #endregion
 
-    /// <summary>
-    /// Awake is called when the script instance is being loaded.
-    /// </summary>
-    void Awake()
+    #region Unity Callbacks
+
+    private void Awake()
     {
         CheckPlayerPrefs();
     }
 
-    public void ChangeSelected()
+    #endregion
+
+    #region Public Methods
+
+    public void ChangeSelectedDifficulty()
     {
         selectedDifficultyIndex = (selectedDifficultyIndex + 1) % difficultyList.Length;
-        ChangeDifficulty(selectedDifficultyIndex);
+        UpdateSelection(ref selectedDifficultyIndex, difficultyList.Select(d => d.DifficultyName).ToArray(), difficultyText);
     }
 
     public void ChangeSelectedMode()
     {
-        selectedModeIndex = (selectedModeIndex + 1) %  Enum.GetValues(typeof(GameMode)).Length;
-        ChangeMode((GameMode) selectedModeIndex);
-    }
-
-    private void ChangeDifficulty(int index)
-    {
-        if(selectedDifficultyIndex != index) {
-            selectedDifficultyIndex = index;
-        }
-        selectedDifficulty = difficultyList[index];
-        difficultyText.text = selectedDifficulty.DifficultyName;
-    }
-
-    private void ChangeMode(GameMode gameMode) {
-        selectedMode = gameMode;
-        modeText.text = Enum.GetNames(typeof(GameMode))[selectedModeIndex];
-    }
-
-    private void CheckPlayerPrefs()
-    {
-        string difficultyName = PlayerPrefs.GetString(Constant.DIFFICULTY_KEY, null);
-        GameMode gameMode = (GameMode) PlayerPrefs.GetInt(Constant.MODE_KEY, 0);
-
-        if (difficultyName != null)
-        {
-            int index = Array.FindIndex(difficultyList, x => x.DifficultyName == difficultyName);
-            ChangeDifficulty(index != -1 ? index : 0);
-        }
-        else
-        {
-            ChangeDifficulty(0);
-        }
-
-        ChangeMode(gameMode);
+        selectedModeIndex = (selectedModeIndex + 1) % Enum.GetValues(typeof(GameMode)).Length;
+        UpdateSelection(ref selectedModeIndex, Enum.GetNames(typeof(GameMode)), modeText);
     }
 
     public void StartGame()
     {
-        PlayerPrefs.SetString(Constant.DIFFICULTY_KEY, selectedDifficulty.DifficultyName);
-        PlayerPrefs.SetInt(Constant.MODE_KEY, (int) selectedMode);
+        PlayerPrefs.SetString(Constant.DIFFICULTY_KEY, difficultyList[selectedDifficultyIndex].DifficultyName);
+        PlayerPrefs.SetInt(Constant.MODE_KEY, selectedModeIndex);
 
         SceneManager.LoadScene(2);
     }
+
+    public void GoToMenu() {
+        SceneManager.LoadScene(0);
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private void UpdateSelection(ref int selectedIndex, string[] items, TMP_Text displayText)
+    {
+        displayText.text = items[selectedIndex];
+    }
+
+    private void CheckPlayerPrefs()
+    {
+        string savedDifficulty = PlayerPrefs.GetString(Constant.DIFFICULTY_KEY, null);
+        GameMode savedMode = (GameMode)PlayerPrefs.GetInt(Constant.MODE_KEY, 0);
+
+        // Update Difficulty
+        int difficultyIndex = savedDifficulty != null 
+            ? Array.FindIndex(difficultyList, d => d.DifficultyName == savedDifficulty) 
+            : 0;
+
+        selectedDifficultyIndex = difficultyIndex != -1 ? difficultyIndex : 0;
+        UpdateSelection(ref selectedDifficultyIndex, difficultyList.Select(d => d.DifficultyName).ToArray(), difficultyText);
+
+        // Update Mode
+        selectedModeIndex = (int)savedMode;
+        UpdateSelection(ref selectedModeIndex, Enum.GetNames(typeof(GameMode)), modeText);
+    }
+
+
+    #endregion
 }
