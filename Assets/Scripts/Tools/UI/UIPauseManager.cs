9bb8c1d8debfd64c003a7d2310de4cf25d661185
Jado <jade.bouiges@edu.esiee-it.fr>
2025-02-03 01:06:08 +0100

    apply modif in mainScene

--- Assets/Scripts/Tools/UI/UIPauseManager.cs
+++ Assets/Scripts/Tools/UI/UIPauseManager.cs
@@ -98,6 +98,8 @@ public class UIPauseManager : MonoBehaviour
     }
     public void OpenSettingsMenu()
     {
+
+        _backbuttonSettings.transform.localScale = new Vector3(0.212207913f, 0.427238166f, 0.212207913f);
         scaleButton = true;
         _settingsCanvas.SetActive(true);
         EventSystem.current.SetSelectedGameObject(_backbuttonSettings);
